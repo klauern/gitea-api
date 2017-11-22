1290aa30bd71df821773782f75f3485a3f6ff558
Nick Klauer <klauer@gmail.com>
2017-11-22 09:36:36 -0600

    stub out using sling

--- client.go
+++ client.go
@@ -2,20 +2,29 @@ package gitea
 
 import "net/http"
 import "time"
+import "github.com/dghubble/sling"
 
 // Client represents an HTTP client to communicate with the Gitea server's API with.
 type Client struct {
-	token string
-	client *http.Client
-	baseUrl string
+	baseURL string
+	token   string
+	sling   *sling.Sling
+	client  *http.Client
+
+	Admin *AdminService
+	Users *UserService
 }
 
-func NewClient(token string, baseUrl string) (*Client) {
-	return &Client {
-		token: token,
-		client: &http.Client {
+func NewClient(token string, baseURL string) *Client {
+	s := sling.New().Base(baseURL)
+	return &Client{
+		baseURL: baseURL,
+		token:   token,
+		sling:   s,
+		client: &http.Client{
 			Timeout: time.Second * 10,
 		},
-		baseUrl: baseUrl,
+
+		Users: NewUserService(s.New()),
 	}
 }
